8dcb80c90b3c493588b895e3702c8f57faed0de0
cpufilecode <zf0908@naver.com>
2023-08-14 21:41:11 +0900

    upload

---
+++ 01/judge2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	printf("Á¤¼ö: ");
+	scanf_s("%d", &n);
+	if (n == 1)
+		puts("A");
+	else if (n == 2)
+		puts("B");
+	else if (n == 3)
+		puts("C");
+	return 0;
+}
