77870261bfa1b7b740f6ba9e4da8b6fe82590c6c
cpufilecode <zf0908@naver.com>
2023-08-14 21:44:09 +0900

    upload

---
+++ 01/judge2x.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	printf("Á¤¼ö: ");
+	scanf_s("%d", &n);
+	if (n == 1)
+		puts("A");
+	else if (n == 2)
+		puts("B");
+	else if (n == 3)
+		puts("C");
+	else
+		;
+	return 0;
+}
