d7fdc52a0bd3a97e2a98518831dd24740a613dc7
cpufilecode <zf0908@naver.com>
2023-08-14 21:19:50 +0900

    upload

--- 01/max3.c
+++ 01/max3.c
@@ -9,6 +9,11 @@ int main(void)
 	printf("b값: "); scanf_s("%d", &b);
 	printf("c값: "); scanf_s("%d", &c);
 
+	int max = a;
+	if (b > max) max = b;
+	if (c > max) max = c;
+
+	printf("최대값은 %d입니다.\n", max);
 
 	return 0;
 }
