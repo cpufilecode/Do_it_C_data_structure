42fa9ed9c4a45b91f962ef3d6a3abf4a84cf36dc
cpufilecode <zf0908@naver.com>
2023-08-14 22:31:10 +0900

    upload

--- 01/multi99table.c
+++ 01/multi99table.c
@@ -4,10 +4,10 @@ int main(void)
 {
 	printf("----- °ö¼ÀÇ¥ -----\n");
 	for (int i = 1; i <= 9; i++) {
-		for (int j = 1; j <= 9; j++) {
+		for (int j = 1; j <= 9; j++) 
 			printf("% 3d", i * j);
-			putchar('\n');
-		}
+		putchar('\n');
+		
 	}
 	
 	return 0;
