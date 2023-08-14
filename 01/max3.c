713d464196a5afc31633fd620460eefd59609b63
cpufilecode <zf0908@naver.com>
2023-08-14 21:18:48 +0900

    upload

--- 01/max3.c
+++ 01/max3.c
@@ -5,6 +5,10 @@ int main(void)
 	int a, b, c;
 
 	printf("세 정수의 최댓값을 구합니다. \n");
+	printf("a값: "); scanf_s("%d", &a);
+	printf("b값: "); scanf_s("%d", &b);
+	printf("c값: "); scanf_s("%d", &c);
+
 
 	return 0;
 }
