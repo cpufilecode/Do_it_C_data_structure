ee477be59fc5f84f9af9190025d542152d9273e6
cpufilecode <zf0908@naver.com>
2023-08-29 22:08:31 +0900

    upload

--- 02/2-4.c
+++ 02/2-4.c
@@ -7,5 +7,19 @@ int main(void)
 	scanf_s("%d", &na);
 	int* a = calloc(na, sizeof(int));
 
+	if (a == NULL)
+		put("메모리 확보에 실패했습니다.");
+	else {
+		printf("%d개의 정수를 입력하세요. \n", na);
+		for (int i = 0; i < na; i++) {
+			printf("a[%d]: ", i);
+			scanf_s("%d", &a[i]);
+		}
+		printf("각 요솟값은 아래와 같습니다.\n");
+		for (int i = 0; i < na; i++)
+			printf("a[%d] = %d\n", i, a[i]);
+		free(a);
+	}
 
+	return 0;
 }
